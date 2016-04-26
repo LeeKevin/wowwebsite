9321ecd80b821346d05c62a144d2849d8a50bf93
Kevin Lee <lee.kev@me.com>
2016-04-25 18:32:01 -0600

    Add contact form

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,13 @@ var elixir = require('laravel-elixir');
 elixir(function (mix) {
     mix
         .sass('main.scss', './public/css')
+        .browserify('main.js', './public/js', null, {
+            paths: ['./node_modules'],
+            debug: !mix.production
+        })
         .copy('./resources/assets/fonts', './public/fonts')
         .copy('./resources/assets/images', './public/images')
-        .copy('./resources/pages/index.html', './public/index.html')
+        .copy('./resources/services', './public/services')
+        .copy('./resources/pages', './public')
     ;
 });
